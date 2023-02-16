11b21d9ad8324238ef7ef08d2a349c966614f2da
Jaylen Harris <harris.j.jaylen22@gmail.com>
2023-02-16 13:16:58 -0600

    Added the numpad

---
+++ calculator.js
@@ -0,0 +1,41 @@
+/*function getcomp (){
+    let rps = ['rock', 'paper', 'scissors'];
+    let choice = rps[Math.floor(Math.random() * rps.length)];
+    console.log(choice);
+}
+getcomp()
+*/
+
+const x = 0;
+const y = 5;
+function add (x, y){
+    let sum = Number(x) + Number(y);
+    return sum;
+};
+
+function subtract (x, y){
+    let difference = x - y;
+    return difference;
+};
+
+function multiply (x, y){
+    let product = x * y;
+    return product;
+};
+
+function divide (x, y){
+    let quotient = x / y;
+    return quotient;
+}
+
+function operate (){
+    let x;
+    let operator;
+    let y;
+
+    
+}
+
+(function hello(word){
+    alert('Hello ' + word)
+})('Jay')
